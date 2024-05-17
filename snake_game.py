22f23539b285332e22c1eeffd7424ea41340d27a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-17 18:09:47 +0100

    Updated snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -15,7 +15,13 @@ class Snake:
     pass
 
 class Food:
-    pass
+    def __init__(self):
+        x = random.randint(0, (GAME_WIDTH // SPACE_SIZE) - 1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT // SPACE_SIZE) - 1) * SPACE_SIZE
+
+        self.coordinates = [x, y]
+
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
 
 def next_turn():
     pass
@@ -49,10 +55,12 @@ window_height = window.winfo_height()
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
-x = int((screen_width/2 - (window_width/2)))
-y = int((screen_height/2 - (window_height/2)))
+x = int((screen_width / 2 - (window_width / 2)))
+y = int((screen_height / 2 - (window_height / 2)))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
+snake = Snake()
+food = Food()
 
 window.mainloop()
