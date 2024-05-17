43b12c59a9bc77ec63c19d5632a5f41559738cd7
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-17 16:52:42 +0100

    Updated snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -33,4 +33,10 @@ window = Tk()
 window.title("Snake game")
 window.resizable(False, False)
 
+score = 0
+direction = "down"
+
+label = Label(window, text="Score:{}".format(score), font=("consolas", 40))
+label.pack()
+
 window.mainloop()
